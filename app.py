352380acca03eae1b054c409d3a7d7ad9b2c57cb
Grant Beasley <grant592@hotmail.com>
2020-10-25 23:21:46 +0000

    first attempt at data table

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import csv
 import pandas as pd
 import numpy as np
 import dash
+import dash_table
 from dash.dependencies import Input, Output, State
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
@@ -37,19 +38,31 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=False
     ),
-    dcc.Graph(id='wcs-heatmap'),
+    html.Div(id='wcs-data'),
+    #dcc.Graph(id='wcs-heatmap'),
 ])
 
 
 @app.callback(
-    Output('wcs-heatmap', 'figure'),
+    Output('wcs-data', 'children'),
     [Input('upload-data', 'contents')])
 def create_heatmap(csv_file):
     content_type, content_string = csv_file.split(',')
     decoded = base64.b64decode(content_string)
     rolling_calculator = RollingAverage(decoded)
     rolling_calculator.apply_all_calculations()
-    return rolling_calculator.plot_heatmap()
+
+    interim = {k: pd.DataFrame(v) for k,v in rolling_calulator.results.items()}
+    table = dash_table.DataTable(
+        id='table',
+        columns=[{"name":i, "id":i} for i in rolling_calculator.results.columns],
+        data=rolling_calculator.results.to_dict('records'),
+    )
+    return html.Div([
+        table,
+        dcc.Graph(id='graph',
+                 figure= rolling_calculator.plot_heatmap())
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
